05ce3837a91061bdabf586f5db3a844ad8dc30f5
浩会海 <50604324+haohuihai@users.noreply.github.com>
2022-07-06 21:54:12 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in users" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  components: {},
+  props: {},
+  setup(props, context) {},
+  mounted() {
+    axios.get("http://localhost:10000/api/users").then((response) => {
+      this.users = response.data;
+    });
+  },
+};
+</script>
+<style scoped>
+</style>
